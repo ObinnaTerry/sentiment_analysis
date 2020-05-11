3c550d52d475f26cb3a7ab040596f187eedc35a8
EC2 Default User <ec2-user@ip-172-16-2-249.us-east-2.compute.internal>
2020-05-11 15:00:29 +0000

    refactor: Add new model parameters n_layers and drop_prob

--- serve/model.py
+++ serve/model.py
@@ -5,14 +5,15 @@ class LSTMClassifier(nn.Module):
     This is the simple RNN model we will be using to perform Sentiment Analysis.
     """
 
-    def __init__(self, embedding_dim, hidden_dim, vocab_size):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, n_layers, drop_prob=0.2):
         """
         Initialize the model by settingg up the various layers.
         """
         super(LSTMClassifier, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=drop_prob)
+        self.dropout = nn.Dropout(drop_prob)
         self.dense = nn.Linear(in_features=hidden_dim, out_features=1)
         self.sig = nn.Sigmoid()
         
@@ -27,6 +28,7 @@ class LSTMClassifier(nn.Module):
         reviews = x[1:,:]
         embeds = self.embedding(reviews)
         lstm_out, _ = self.lstm(embeds)
+        lstm_out = self.dropout(lstm_out)
         out = self.dense(lstm_out)
         out = out[lengths - 1, range(len(lengths))]
         return self.sig(out.squeeze())
